752d0fdecfe6ad9a66c8169d76a2ff9ec3c02678
Koushik55555 <koushikm.135@gmail.com>
2018-08-20 17:27:05 +0530

    7 commit

--- TrainingPackage/Hello.java
+++ TrainingPackage/Hello.java
@@ -7,6 +7,10 @@ public class Hello
             System.out.println("Hello world"+i);
             Add a = new Add();
             System.out.println(a.add(i,i));
+            for(int j=0:j=i:j++)
+            {
+                System.out.println("Inner loop"+j);
+            }
         }
     }
 }
